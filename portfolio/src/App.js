a1fb7761a18fcdc506888319d01c1e3671fc380a
mguppy <mguppy34@gmail.com>
2021-08-24 09:03:09 -0500

    about page rendering with tabs

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import PortfolioContainer from "./components/PortfolioContainer";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+//         <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+//           <div class="navbar-nav">
+//             <a class="nav-item nav-link active" href="#">About Me <span class="sr-only"></span></a>
+//             <a class="nav-item nav-link" href="#">Portfolio</a>
+//             <header className = "navheader">Maryam Guppy</header>
+//             <a class="nav-item nav-link" href="#">Contact</a>
+//             <a class="nav-item nav-link disabled" href="#">Resume</a>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
 
 export default App;
