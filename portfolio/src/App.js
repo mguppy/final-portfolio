39c52875f199d9d8aff0d97f397323d13249c771
mguppy <mguppy34@gmail.com>
2021-08-25 15:50:01 -0500

    Updates

--- portfolio/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import PortfolioContainer from "./components/PortfolioContainer";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const App = () => <PortfolioContainer />;
-
-export default App;
