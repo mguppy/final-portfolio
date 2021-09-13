ee04b2e167320fa90ee3e579f78dee493b4d66c7
mguppy <mguppy34@gmail.com>
2021-09-13 12:12:13 -0500

    added deployed app and code links for all projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,45 +15,41 @@ export default function Projects() {
                 <Row className="m-auto align-self-center">
                     <Col>
                         <Card>
-                            <a href="https://brandonnorsworthy.github.io/pet-seeker/" target="_blank">
                                 <Card.Img className="project-photo" variant="top" src="./Images/pet-seeker.png" />
                                 <Card.Body>
-                                    <Card.Text className="carousel-title">
-                                        Pet Seeker Application
+                                    <Card.Text className="text-center">
+                                        <a className="project-btn" href="https://brandonnorsworthy.github.io/pet-seeker/" target="_blank">Deployed Application</a>
+                                        <a className="project-btn" href="https://github.com/mguppy/pet-seeker" target="_blank">Code</a>
                                     </Card.Text>
                                 </Card.Body>
-                            </a>
                         </Card>
                         <br />
                         <Card>
-                            <a href="https://jobster-group-2.herokuapp.com/aboutus" target="_blank">
                                 <Card.Img className="project-photo" variant="top" src="./Images/jobster.png" />
                                 <Card.Body>
-                                    <Card.Text className="carousel-title">
-                                        Jobster Application
+                                    <Card.Text className="text-center">
+                                        <a className="project-btn" href="https://jobster-group-2.herokuapp.com/aboutus" target="_blank">Deployed Application</a>
+                                        <a className="project-btn" href="https://github.com/mguppy/Jobster" target="_blank">Code</a>
                                     </Card.Text>
                                 </Card.Body>
-                            </a>
                         </Card>
                         <Card>
-                            <a href="https://mguppy.github.io/code-quiz/" target="_blank">
                                 <Card.Img className="project-photo" variant="top" src="./Images/code-quiz.png" />
                                 <Card.Body>
-                                    <Card.Text className="carousel-title">
-                                        Code Quiz Application
+                                    <Card.Text className="text-center">
+                                        <a className="project-btn" href="https://mguppy.github.io/code-quiz/" target="_blank">Deployed Application</a>
+                                        <a className="project-btn" href="https://github.com/mguppy/code-quiz" target="_blank">Code</a>
                                     </Card.Text>
                                 </Card.Body>
-                            </a>
                         </Card>
                         <Card>
-                            <a href="https://www.eridium.chat/" target="_blank">
                                 <Card.Img className="project-photo" variant="top" src="./Images/eridium.png" />
                                 <Card.Body>
-                                    <Card.Text className="carousel-title">
-                                        Eridium Application
+                                    <Card.Text className="text-center">
+                                        <a className="project-btn" href="https://www.eridium.chat/" target="_blank">Deployed Application</a>
+                                        <a className="project-btn" href="https://github.com/mguppy/eridium" target="_blank">Code</a>
                                     </Card.Text>
                                 </Card.Body>
-                            </a>
                         </Card>
                     </Col>
                 </Row>
